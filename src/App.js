d00bbaf25b34820dbf465a305e2e3e0570ee2a7f
RUGUMBIRA Jordy Bastien <jordybastien@JORDY-BASTIENs-MacBook-Pro.local>
2019-10-30 12:31:58 +0200

    setting up webpack

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./css/style.css";
+class App extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>My App</h1>
+            </div>
+         );
+    }
+}
+ 
+export default App;
